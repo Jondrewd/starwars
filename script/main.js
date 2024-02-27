c90981f30b6cd4a168a509ffd07aac55e7d25a23
Jondrewd <jonas220703@gmail.com>
2024-02-27 10:56:47 -0300

    arrumando as imagens

--- script/main.js
+++ script/main.js
@@ -25,10 +25,10 @@ function createSelectWorldEl(a,i) {
     d1.classList.add('select-world')
     const i1 = document.createElement('img')
     i1.setAttribute('alt', `Escolha o anime ${a}`)
-    i1.setAttribute('src', `/assets/lado/${a}/wpp/${a}_${data[i]['wpp'][0]}`)
+    i1.setAttribute('src', `assets/lado/${a}/wpp/${a}_${data[i]['wpp'][0]}`)
     const i2 = document.createElement('img')
     i2.setAttribute('alt', a)
-    i2.setAttribute('src', `/assets/lado/${a}/${a}_logo.png`)
+    i2.setAttribute('src', `assets/lado/${a}/${a}_logo.png`)
 
     d1.appendChild(i1)
     d1.appendChild(i2)
@@ -42,7 +42,7 @@ function creatorCharacterSelectorEl(a, i, c) {
     const s1 = document.createElement('span')
     s1.innerText = data[i]['name'][c]
     const i1 = document.createElement('img')
-    i1.setAttribute('src', `/assets/lado/${a}/tokens/${a}_${data[i]['tokens'][c]}`)
+    i1.setAttribute('src', `assets/lado/${a}/tokens/${a}_${data[i]['tokens'][c]}`)
     d1.appendChild(s1)
     d1.appendChild(i1)
 
@@ -59,7 +59,7 @@ function windowViewBg() {
 }
 
 function setContent(a, i, c=0) {
-    contentImg.setAttribute('src', `/assets/lado/${a}/${a}_logo.png`)
+    contentImg.setAttribute('src', `assets/lado/${a}/${a}_logo.png`)
     contentH2.innerText = data[i]['name'][c]
     contentP.innerText = data[i]['desc'][c]
 }
@@ -70,7 +70,7 @@ function changeChar(a, i, c) {
     setTimeout(() => {
         contentH2.innerText = data[i]['name'][c]
         contentP.innerText = data[i]['desc'][c]  
-        viewBg.style.background = `url(/assets/lado/${a}/wpp/${a}_${data[i]['wpp'][c]})`
+        viewBg.style.background = `url(assets/lado/${a}/wpp/${a}_${data[i]['wpp'][c]})`
         viewBg.style.backgroundSize = 'cover'
         viewBg.style.backgroundPosition = '20%'      
     }, 500);
@@ -90,7 +90,7 @@ for(let index = 0; index < data.length; index++) {
         for(let char = 0; char < data[index]['name'].length; char++) {
             nav.appendChild(creatorCharacterSelectorEl(a, index, char))
         }
-        viewBg.style.background = `url(/assets/lado/${a}/wpp/${a}_${data[index]['wpp'][0]})`
+        viewBg.style.background = `url(assets/lado/${a}/wpp/${a}_${data[index]['wpp'][0]})`
         viewBg.style.backgroundSize = 'cover'
         viewBg.style.backgroundPosition = 'center'
         viewBg.style.width = window.innerWidth + 'px'
